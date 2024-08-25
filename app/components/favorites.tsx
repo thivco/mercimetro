7d74167035667985eb04a9793f6c9a135ee8cca8
thivco <thibault.vivier@epitech.eu>
2024-08-25 16:41:58 +0200

    Added favorites tab that parses through the fav tabs

--- app/components/favorites.tsx
+++ app/components/favorites.tsx
@@ -1,7 +1,29 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState } from 'react';
+import { Text } from 'react-native';
+
 export default function Favorites() {
+    
+    const [currentFavorites, setCurrentFavorites] = useState(undefined)
+
+    async function getFavorites(){
+        let tempFav = await AsyncStorage.getItem("favorites")
+        setCurrentFavorites(JSON.parse(tempFav))
+        console.log("Temp favorites", JSON.parse(tempFav));
+    }
+    // getFavorites()
+
     return(
         <>
-     <p>Favorites et tout la</p>
+        <p>Test favoris</p>
+        {/* En vrai ca serait mieux de faire la regeneration au clic sur favorites, mais bon.................. */}
+        <Text onPress={() => getFavorites()}>Regenerate favorites</Text>
+            {currentFavorites && currentFavorites.map((favorite, index) => {
+                return (
+                    <p>DESTINATION : {favorite.destination}</p>
+                )
+                
+            })}        
         </>
     )
 }
